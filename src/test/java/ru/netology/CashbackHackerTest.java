87af3f5a803e522c553aa3bbcf3bbeb49d9798f7
firuz kholmatov <firuzkholmatov@yandex.ru>
2022-06-04 16:55:49 +0300

    JUnit4 added

--- src/test/java/ru/netology/CashbackHackerTest.java
+++ src/test/java/ru/netology/CashbackHackerTest.java
@@ -1,9 +1,9 @@
 package ru.netology;
 
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackerTest {
     CashbackHacker service = new CashbackHacker();
