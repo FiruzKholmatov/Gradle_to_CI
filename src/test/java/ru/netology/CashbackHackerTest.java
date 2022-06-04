5b4641089b5c2990780561517843b7d4a66c1b95
firuz kholmatov <firuzkholmatov@yandex.ru>
2022-06-04 16:44:43 +0300

    class and tests added

---
+++ src/test/java/ru/netology/CashbackHackerTest.java
@@ -0,0 +1,51 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CashbackHackerTest {
+    CashbackHacker service = new CashbackHacker();
+
+
+    @Test
+    public void shouldCalculateRest() {
+
+        int actual = service.remain(650);
+        int expected = 350;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateRestAtBound() {
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void shouldCalculateRestOutOfBound() {
+
+        int actual = service.remain(1800);
+        int expected = 200;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void shouldCalculateWithNegative() {
+
+        int actual = service.remain(-10);
+        int expected = 1010;
+
+        assertEquals(actual, expected);
+
+    }
+
+
+}
